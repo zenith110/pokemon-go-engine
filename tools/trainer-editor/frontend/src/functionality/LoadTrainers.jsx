bbdb54d317ac8d5c67a9ac2697500b61d04647d2
zenith110 <thequake@live.com>
2023-12-28 21:39:57 -0500

    Started working on tools

---
+++ tools/trainer-editor/frontend/src/functionality/LoadTrainers.jsx
@@ -0,0 +1,4 @@
+const LoadTrainers = () => {
+
+}
+export default LoadTrainers
---
+++ tools/trainer-editor/frontend/src/functionality/TrainerCard.jsx
@@ -0,0 +1,31 @@
+import PokemonSelection from "./PokemonSelection"
+import PokemonStats from "./PokemonStats";
+import { useState } from "react"
+const TrainerCard = ({ trainerName, trainerId, pokemons, sprite, location, nextFightId }) => {
+const [currentlySelectedPokemon, setCurrentlySelectedPokemon ] = useState(null);
+const [visibleStats, setVisibleStats ] = useState(false)
+const [pokemonData, setPokemonData ] = useState([])
+return(
+    <>
+    <p>
+    <label>Name: </label>
+    <p>{trainerName}</p>
+    <br/>
+    <label>Id: </label>
+    <p>{trainerId}</p>
+    <img src={sprite} />
+    {pokemons.maps((pokemon, index) => {
+        <>
+        <PokemonSelection pokemonData={pokemon} setCurrentlySelectedPokemon={setCurrentlySelectedPokemon} setVisible={setVisibleStats} index={index} setPokemonData={setPokemonData}/>
+        </>
+    })}
+    <label>Found in: </label>
+    <p>{location}</p>
+    <br/>
+    <label>Next Battle: </label>
+    <p>{nextFightId}</p>
+    </p>
+    </>
+)
+}
+export default TrainerCard
