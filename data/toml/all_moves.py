c3476aac3a292f9f297c5d65ac52d02c2dc1b0a4
zenith110 <thequake@live.com>
2024-02-29 17:31:20 -0500

    Added all pokemon moves, updated eeveelutions

---
+++ data/toml/all_moves.py
@@ -0,0 +1,46 @@
+import pokebase as pb
+from pokebase import cache
+import toml
+import re
+cache.API_CACHE
+
+data = []
+all_moves = pb.APIResourceList("move")
+
+for move_data in all_moves:
+    # Get API data for this move.
+    move = pb.move(move_data["name"])
+    effect_entry = []
+    descriptions = []
+    try:
+        for description in move.flavor_text_entries:
+            if(description.language.name == "en"):
+                descriptions.append({
+                    "description": description.flavor_text}
+                    )
+        for effect_data in move.effect_entries:
+            if(effect_data.language.name == "en"):
+                effect_description = effect_data.effect
+                effect_entry.append({
+                    "effect_text": effect_description
+                })
+    except Exception as e:
+        print(f"Skipping, error is {e}")
+    move_data = {
+        "name": move.name,
+        "Power": move.power,
+        "pp": move.pp,
+        "accuracy": move.accuracy,
+        "descriptions": descriptions,
+        "type": move.type,
+        "kind_of_move": move.damage_class.name,
+        "effects": effect_entry
+    }
+    data.append(move_data)
+    pokemon_result = {
+        "move": data
+    }
+    print(pokemon_result)
+    output_file_name = f"moves.toml"
+    with open(output_file_name, "w", encoding="utf-8") as toml_file:
+        toml.dump(pokemon_result, toml_file)
