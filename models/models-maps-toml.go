d4a3341a33927f1702d5717623fc1a89ff9255cc
zenith110 <thequake@live.com>
2024-04-11 19:55:01 -0400

    Added map code

---
+++ models/models-maps-toml.go
@@ -0,0 +1,24 @@
+package models
+
+type Maps struct {
+	Map Map `toml:"map"`
+}
+type MapPokemon struct {
+	Species      string `toml:"species"`
+	MaxLevel     string `toml:"maxLevel"`
+	MinLevel     string `toml:"minLevel"`
+	PriorityTile string `toml:"priorityTile"`
+}
+type Encounter struct {
+	Pokemon MapPokemon `toml:"pokemon"`
+}
+type Map struct {
+	Name        string    `toml:"name"`
+	Filepath    string    `toml:"filepath"`
+	Tilesetpath string    `toml:"tilesetpath"`
+	Description string    `toml:"description"`
+	TypeOfMap   string    `toml:"typeOfMap"`
+	BgMusic     string    `toml:"bgMusic"`
+	ID          string    `toml:"id"`
+	Encounter   Encounter `toml:"encounter"`
+}
