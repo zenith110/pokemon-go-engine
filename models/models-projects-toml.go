4bd2c065d5a25657442b3239e95a4d9501a0d9d6
zenith110 <thequake@live.com>
2024-04-12 21:39:52 -0400

    Updated to remove inline struct for projects

--- models/models-projects-toml.go
+++ models/models-projects-toml.go
@@ -1,10 +1,11 @@
 package models
 
 type Projects struct {
-	Project struct {
-		Name            string `toml:"name"`
-		FolderLocation  string `toml:"folderLocation"`
-		VersionOfEngine string `toml:"versionOfEngine"`
-		CreatedDateTime string `toml:"createdDateTime"`
-	} `toml:"project"`
+	Project Project `toml:"project"`
+}
+type Project struct {
+	Name            string `toml:"name"`
+	FolderLocation  string `toml:"folderLocation"`
+	VersionOfEngine string `toml:"versionOfEngine"`
+	CreatedDateTime string `toml:"createdDateTime"`
 }
