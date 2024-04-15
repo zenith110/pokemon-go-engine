b44cf3a308ede2f8b9a98adc4560ca90194887ed
zenith110 <thequake@live.com>
2024-04-15 12:52:14 -0400

    Added model for overworlds

---
+++ models/models-overworld-toml.go
@@ -0,0 +1,46 @@
+package models
+
+type OverworldData struct {
+	Overworlds []Overworld `toml:"overworld"`
+}
+type Up struct {
+	ID        int    `toml:"id"`
+	FrameName string `toml:"frameName"`
+}
+type Down struct {
+	ID        int    `toml:"id"`
+	FrameName string `toml:"frameName"`
+}
+type Right struct {
+	ID        int    `toml:"id"`
+	FrameName string `toml:"frameName"`
+}
+type Left struct {
+	ID        int    `toml:"id"`
+	FrameName string `toml:"frameName"`
+}
+type Swimming struct {
+	Up    []Up    `toml:"up"`
+	Down  []Down  `toml:"down"`
+	Right []Right `toml:"right"`
+	Left  []Left  `toml:"left"`
+}
+type Walking struct {
+	Up    []Up    `toml:"up"`
+	Down  []Down  `toml:"down"`
+	Left  []Left  `toml:"left"`
+	Right []Right `toml:"right"`
+}
+type Running struct {
+	Up    []Up    `toml:"up"`
+	Down  []Down  `toml:"down"`
+	Left  []Left  `toml:"left"`
+	Right []Right `toml:"right"`
+}
+type Overworld struct {
+	Name     string   `toml:"name"`
+	ID       string   `toml:"id"`
+	Swimming Swimming `toml:"swimming"`
+	Walking  Walking  `toml:"walking"`
+	Running  Running  `toml:"running"`
+}
