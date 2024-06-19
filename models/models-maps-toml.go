f0f1af6d3f5ce63c18270dda085a80c301642573
zenith110 <thequake@live.com>
2024-06-18 21:43:45 -0400

    Added new toml data for a singular map

--- models/models-maps-toml.go
+++ models/models-maps-toml.go
@@ -1,24 +1,11 @@
 package models
 
-type Maps struct {
-	Map []Map `toml:"map"`
+type MapData struct {
+	Maps []Map `toml:"map"`
 }
-type MapPokemon struct {
-	Species      string `toml:"species"`
-	MaxLevel     string `toml:"maxLevel"`
-	MinLevel     string `toml:"minLevel"`
-	PriorityTile string `toml:"priorityTile"`
-}
-type Encounter struct {
-	Pokemon MapPokemon `toml:"pokemon"`
-}
+
 type Map struct {
-	Name        string    `toml:"name"`
-	Filepath    string    `toml:"filepath"`
-	Tilesetpath string    `toml:"tilesetpath"`
-	Description string    `toml:"description"`
-	TypeOfMap   string    `toml:"typeOfMap"`
-	BgMusic     string    `toml:"bgMusic"`
-	ID          string    `toml:"id"`
-	Encounter   Encounter `toml:"encounter"`
+	Name     string `toml:"name"`
+	Filepath string `toml:"filepath"`
+	ID       string `toml:"id"`
 }
