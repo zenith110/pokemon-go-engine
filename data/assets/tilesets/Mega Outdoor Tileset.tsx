33da616bdda678510bd6425d34146ebdf63324a8
zenith110 <thequake@live.com>
2025-06-05 21:32:34 -0400

    Updated data

--- data/assets/tilesets/Mega Outdoor Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Mega Outdoor Tileset" tilewidth="16" tileheight="16" tilecount="1800" columns="8">
- <image source="Ekats Public Tiles/Ekat's Public Tiles/Mega Outdoor Tileset.png" width="128" height="3600"/>
-</tileset>
--- data/assets/tilesets/Route 224.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Route 224" tilewidth="16" tileheight="16" tilecount="272" columns="8">
- <image source="Ekats Public Tiles/Ekat's Public Tiles/Route 224.png" width="128" height="544"/>
-</tileset>
